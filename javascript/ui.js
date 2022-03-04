a62d498d97948a8f435b52717cdb10787ca3054e
jonetxe13 <jonetxeba.je10@gmail.com>
2022-03-04 18:56:13 +0100

    rock-paper-scissorcs functional

--- javascript/ui.js
+++ javascript/ui.js
@@ -1,32 +1,30 @@
-				function computerPlay(){
-					const i = Math.floor(Math.random() * (4 - 1)) + 1;
-					let computer = "tijeras";
-					if (i == 1){
-						 computer = "piedra";
-					}
-					if(i == 2){
- 						 computer = "papel";
-					}
-					return computer;
-				}
-				function playRound(playerSelection, computerSelection) {
-					let res = "has ganado";
-					if (playerSelection == computerSelection){
- 							res = "empate";
-					}
-					if (playerSelection == "piedra" & computerSelection == "papel"){
- 							res = "el ordenador gana";
-					}
-					if (playerSelection == "papel" & computerSelection == "tijeras"){
- 							res = "el ordenador gana";
-					}
-					if (playerSelection == "tijeras" & computerSelection == "piedra"){
- 							res = "el ordenador gana";
-					}
-					//console.log("el resultado es: " + res);
-					return res;
-				}
-let computerSelection = computerPlay();
+function computerPlay() {
+  const i = Math.floor(Math.random() * (4 - 1)) + 1;
+  let computer = "tijeras";
+  if (i == 1) {
+    computer = "piedra";
+  }
+  if (i == 2) {
+    computer = "papel";
+  }
+  return computer;
+}
+function playRound(playerSelection, computerSelection) {
+  let res = "has ganado";
+  if (playerSelection == computerSelection) {
+    res = "empate";
+  }
+  if ((playerSelection == "piedra") & (computerSelection == "papel")) {
+    res = "el ordenador gana";
+  }
+  if ((playerSelection == "papel") & (computerSelection == "tijeras")) {
+    res = "el ordenador gana";
+  }
+  if ((playerSelection == "tijeras") & (computerSelection == "piedra")) {
+    res = "el ordenador gana";
+  }
+  return res;
+}
 
 const container1 = document.querySelector(".container1");
 
@@ -35,9 +33,6 @@ const papel = document.createElement("button");
 const tijeras = document.createElement("button");
 
 container1.style.textAlign = "center";
-//piedra.className = "piedra";
-//papel.className = "papel";
-//tijeras.className = "tijeras";
 
 piedra.textContent = "piedra";
 papel.textContent = "papel";
@@ -55,19 +50,81 @@ container1.appendChild(piedra);
 container1.appendChild(papel);
 container1.appendChild(tijeras);
 
-
 //results and current score of the game
 const container2 = document.querySelector(".container2");
+const container3 = document.querySelector(".container3");
 
-piedra.addEventListener('click', () => {
-	container2.textContent = playRound("piedra", computerSelection);
-	console.log(playRound("piedra", computerSelection));
-});
-papel.addEventListener('click', () => {
-	container2.textContent = (playRound("papel", computerSelection));
-	console.log(playRound("papel", computerSelection));
-});
-tijeras.addEventListener('click', () => {
-	container2.textContent = (playRound("tijeras", computerSelection));
-	console.log(playRound("tijeras", computerSelection));
-});
+let wscore = 0;
+let lscore = 0;
+
+function game() {
+  piedra.onclick = () => {
+    let computerSelection = computerPlay();
+    container2.textContent = playRound("piedra", computerSelection);
+    if (container2.textContent == "has ganado") {
+      wscore++;
+    }
+    if (container2.textContent == "el ordenador gana") {
+      lscore++;
+    }
+    container3.textContent = "puntos ganados: " + wscore + " puntos perdidos: " + lscore;
+    if (wscore == 5) {
+      container3.textContent = "Has ganado";
+      wscore = 0;
+      lscore = 0;
+    }
+    if (lscore == 5) {
+      container3.textContent = "Has perdido";
+      wscore = 0;
+      lscore = 0;
+    }
+
+  };
+  papel.onclick = () => {
+    let computerSelection = computerPlay();
+    container2.textContent = playRound("papel", computerSelection);
+    if (container2.textContent == "has ganado") {
+      wscore++;
+    }
+    if (container2.textContent == "el ordenador gana") {
+      lscore++;
+    }
+    container3.textContent = "puntos ganados: " + wscore + " puntos perdidos: " + lscore;
+    if (wscore == 5) {
+      container3.textContent = "Has ganado";
+      wscore = 0;
+      lscore = 0;
+    }
+    if (lscore == 5) {
+      container3.textContent = "Has perdido";
+      wscore = 0;
+      lscore = 0;
+    }
+  };
+  tijeras.onclick = () => {
+    let computerSelection = computerPlay();
+    container2.textContent = playRound("tijeras", computerSelection);
+    if (container2.textContent == "has ganado") {
+      wscore++;
+    }
+    if (container2.textContent == "el ordenador gana") {
+      lscore++;
+    }
+    container3.textContent = "puntos ganados: " + wscore + " puntos perdidos: " + lscore;
+    if (wscore == 5) {
+      container3.textContent = "Has ganado";
+      wscore = 0;
+      lscore = 0;
+    }
+    if (lscore == 5) {
+      container3.textContent = "Has perdido";
+      wscore = 0;
+      lscore = 0;
+    }
+  };
+}
+game();
+
+container2.style.textAlign = "center";
+container3.style.textAlign = "center";
+
