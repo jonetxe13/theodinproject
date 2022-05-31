2bbbe71ad1ace2c67b9fc87e6a1616243426117f
jonetxe13 <jonetxeba.je10@gmail.com>
2022-05-31 19:05:56 +0200

    boton se cierra y la foto esta bn

--- pagina-prueba/portfolio1/pages/cover.js
+++ pagina-prueba/portfolio1/pages/cover.js
@@ -1,17 +1,26 @@
 import Image from "next/image";
+import Careto from "../public/fotocurriculum.jpg";
 
 export default function Cover() {
   return (
-    <div className="bg-gradient-to-br from-gray-100 to-gray-800 border-2 border-gray-700 rounded-2xl m-20 p-14 flex sm:flex-row flex-col text-center mt-60 shadow-gray-900 shadow-2xl">
-      <Image
-        src="/fotocurriculum.jpg"
-        width="190vh"
-        height="190vh"
-        className="max-w-auto max-h-auto rounded-full"
-      />
+    <div className="bg-gradient-to-br from-gray-500 to-gray-800 border-2 border-gray-700 rounded-2xl my-20 lg:mx-72 md:mx-32 mx-14 md:py-32 py-12 px-10 flex md:flex-row flex-col text-center shadow-gray-900 shadow-2xl justify-center">
+      <div className="w-48 h-auto self-center">
+        <Image
+          src={Careto}
+          width={170}
+          height={170}
+          layout="responsive"
+          className="rounded-full "
+        />
+      </div>
       <div className="m-6 flex-col text-center">
-        <h1 className="text-2xl text-gray-200 font-bold">Jon Etxebarria Sasia</h1>
-        <p className="text-lg text-gray-200 mt-5 text-center">Actualmente estoy cursando el primer curso del grado de informatica en la UPV ehu</p>
+        <h1 className="text-2xl text-gray-200 font-bold">
+          Jon Etxebarria Sasia
+        </h1>
+        <p className="text-lg text-gray-200 mt-5 text-center">
+          Actualmente estoy cursando el primer curso del grado de informatica en
+          la UPV ehu
+        </p>
       </div>
     </div>
   );
--- pagina-prueba/portfolio1/pages/header.js
+++ pagina-prueba/portfolio1/pages/header.js
@@ -1,32 +1,58 @@
-export default function Header(){
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
-  function HeaderOptions({text}){
-    return (<button className='flex-1 hover:shadow-md hover:shadow-gray-800 font-bold text-gray-700 whitespace-nowrap hover:text-white hover:bg-gray-700 rounded-lg mx-4 hover:cursor-pointer p-4 transition-all active:translate-y-1'>
-            {text}
-          </button>);
-  }  
-  // function ToggleShrink(){
-  //   if(useEffect(window.innerWidth)<= 400){
-  //     return (
-  //       <Imagen/>
-  //     )
-  //   }
-  //   else{
-  //     return(
-  //     )
-  //   }
-  // }
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+  });
+  useEffect(() => {
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth
+      });
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return windowSize;
+}
+
+
+export default function Header() {
+  function HeaderOptions({ text, link }) {
+    return (
+      <button className="flex-1 hover:shadow-md hover:shadow-gray-800 font-bold text-gray-700 whitespace-nowrap hover:text-white hover:bg-gray-700 rounded-lg md:mx-4 mx-2 hover:cursor-pointer md:p-4 p-3 transition-all active:translate-y-1">
+        <a href={link}>{text}</a>
+      </button>
+    );
+  }
+
+  function ClosedOrOpen() {
+    const size = useWindowSize();
+    if (size.width < 600) {
+      return <Image src="/menu.png" height="20px" width="20px" />;
+    } else {
+      return (
+        <>
+          <HeaderOptions link="#resumen" text="BLOG" />
+          <HeaderOptions text="ABOUT ME" />
+          <HeaderOptions text="CONTACT" />
+        </>
+      );
+    }
+  }
 
   return (
-    <div className='bg-gradient-to-br from-gray-100 to-gray-600 flex p-4 shadow-gray-900 shadow-lg'>
-      <div className='cursor-default bg-gray-700 font-extrabold text-3xl p-4 text-center text-white shadow-md shadow-gray-700 rounded-lg'>
+    <div className="bg-gradient-to-br from-gray-100 to-gray-600 flex p-4 shadow-gray-900 shadow-lg">
+      <div className="cursor-default bg-gray-700 font-extrabold text-3xl p-4 text-center text-white shadow-md shadow-gray-700 rounded-lg">
         JN
       </div>
-      <div className='flex items-center text-right ml-auto '>
-        <HeaderOptions text="BLOG"/>
-        <HeaderOptions text="ABOUT ME"/>
-        <HeaderOptions text="CONTACT"/>
+      <div className="flex items-center text-right md:ml-auto ml-auto">
+        <ClosedOrOpen />
       </div>
     </div>
-  )
+  );
 }
